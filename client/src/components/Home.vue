72de5c4be9e13bde1a91d19aed57bd03980c5ffa
Rudy Wahjudi <rudy.wahjudi@gmail.com>
2017-05-18 10:49:14 +0700

    setting up Home landing page

---
+++ client/src/components/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="">
+    {{message}}
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex'
+
+export default {
+  computed: mapGetters({
+    message: 'getMessage'
+  }),
+  methods: {
+
+  }
+} // end of export
+</script>
+
+<style scoped>
+</style>
---
+++ client/src/main.js
@@ -0,0 +1,20 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import store from './vuex/store'
+
+require('semantic-ui-css/semantic.css')
+require('semantic-ui-css/semantic.js')
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  store,
+  router,
+  template: '<App/>',
+  components: { App }
+})
